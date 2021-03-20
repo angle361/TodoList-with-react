28df9bc535a521a650c025f3cad9555fffadd4bc
kittu-20 <sanyam.student.mec19@itbhu.ac.in>
2021-03-21 00:28:28 +0530

    initial commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function TodoItem(props) {
+  // const [isclicked, setIsclicked] = useState(false);
+
+  // function handleClick(){
+  //   setIsclicked( prevValue => {
+  //      return !prevValue;
+  //   });
+  // }
+
+  return (
+    <div
+      onClick={function () {
+        return props.onChecked(props.id);
+      }}
+    >
+      <li>{props.item}</li>
+    </div>
+  );
+}
+export default TodoItem;
