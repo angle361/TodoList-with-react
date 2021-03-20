e332a3d675aa7c16687ea45966523e9c6b45fe99
Sanyam Aggarwal <70066433+angle361@users.noreply.github.com>
2021-03-21 01:20:39 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,7 +32,7 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
+        <input onChange={handleChange} type="text" value={inputText} required />
         <button onClick={addItem}>
           <span>Add</span>
         </button>
